4dd87a24fde959b7e13dd179911b9f8c64b0220a
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-11-08 13:59:15 -0300

    Add prisma answer attachments repository

--- src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
@@ -1,18 +1,29 @@
-// import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
-// import { Injectable } from '@nestjs/common'
-// import { PrismaService } from '../prisma.service'
+import { Injectable } from '@nestjs/common'
+import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
+import { PrismaService } from '../prisma.service'
+import { PrismaAnswerAttachmentMapper } from '../mappers/prisma-answer-attachment-mapper'
 
-// @Injectable()
-// export class PrismaAnswerAttachmentsRepository
-//   implements AnswerAttachmentsRepository
-// {
-//   constructor(private readonly prisma: PrismaService) {}
+@Injectable()
+export class PrismaAnswerAttachmentsRepository
+  implements AnswerAttachmentsRepository
+{
+  constructor(private readonly prisma: PrismaService) {}
 
-//   async findManyByAnswerId(answerId: string) {
-//     throw new Error('Method not implemented.')
-//   }
+  async findManyByAnswerId(answerId: string) {
+    const answerAttachments = await this.prisma.attachment.findMany({
+      where: {
+        answerId,
+      },
+    })
 
-//   async deleteManyByAnswerId(answerId: string) {
-//     throw new Error('Method not implemented.')
-//   }
-// }
+    return answerAttachments.map(PrismaAnswerAttachmentMapper.toDomain)
+  }
+
+  async deleteManyByAnswerId(answerId: string) {
+    await this.prisma.attachment.deleteMany({
+      where: {
+        answerId,
+      },
+    })
+  }
+}
