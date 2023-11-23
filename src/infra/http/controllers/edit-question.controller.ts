69d391ec87a1e051c17abac7b0a755c9f212f6ca
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-11-23 12:07:23 -0300

    Add edit question controller + change repos at domain level to abstract classes

---
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -0,0 +1,49 @@
+import { z } from 'zod'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+
+const editQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>
+
+const bodyValidatitonPipe = new ZodValidationPipe(editQuestionBodySchema)
+@Controller('/questions/:id')
+export class EditQuestionController {
+  constructor(private readonly editQuestion: EditQuestionUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @Body(bodyValidatitonPipe) body: EditQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('id') questionId: string,
+  ) {
+    const userId = user.sub
+    const { title, content } = body
+
+    const result = await this.editQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
+      questionId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
