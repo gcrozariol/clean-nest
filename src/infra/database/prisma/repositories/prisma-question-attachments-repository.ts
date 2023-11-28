151bccd88d8b9c61a1baf455df16a84203662c89
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-11-28 20:41:15 -0300

    Implement createMany question attachments under infra layer (Prisma)

--- src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
@@ -2,6 +2,7 @@ import { QuestionAttachmentsRepository } from '@/domain/forum/application/reposi
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaQuestionAttachmentMapper } from '../mappers/prisma-question-attachment-mapper'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 
 @Injectable()
 export class PrismaQuestionAttachmentsRepository
@@ -17,6 +18,20 @@ export class PrismaQuestionAttachmentsRepository
     })
 
     return questionAttachments.map(PrismaQuestionAttachmentMapper.toDomain)
+  }
+
+  async createMany(attachments: QuestionAttachment[]) {
+    if (attachments.length === 0) {
+      return
+    }
+
+    const data = PrismaQuestionAttachmentMapper.toPrismaUpdateMany(attachments)
+
+    await this.prisma.attachment.updateMany(data)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]) {
+    throw new Error('Method not implemented.')
   }
 
   async deleteManyByQuestionId(questionId: string) {
